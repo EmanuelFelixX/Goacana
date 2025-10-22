2ce57663f425c3850ba802a6684c3e929276dce5
Érrylan <estudos82020@gmail.com>
2025-10-22 20:00:56 -0300

    Alterações feitas no Javascript de contatos.html

--- core/static/JS/contato.js
+++ core/static/JS/contato.js
@@ -1,10 +1,10 @@
 function enviarWhats(){
-    event.preventDefault();
     const nome = document.getElementById('nome').value;
     const assunto = document.getElementById('assunto').value;
     const telefone = '558491061685';
-    const mensagem = `Olá, meu nome é ${nome}. Gostaria de falar sobre ${assunto}.`;
-    const msgFormatada = encodeURIComponent(mensagem);
+    const mensagem = document.getElementById('mensagem').value;
+    const texto = `Olá, meu nome é ${nome}. Gostaria de falar sobre ${assunto}.: ${mensagem}`;
+    const msgFormatada = encodeURIComponent(texto);
     const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
     window.open(url, '_blank');
 
